b5f4af651613c723e595f182145a9c19f91c27ad
EdwinWilliamArevalo <edwin.arevalo@tecsup.edu.pe>
2020-06-21 02:25:45 -0500

    finish

---
+++ Semana11/Semana11/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Semana11.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana11
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new SimpleDemo());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new InteractiveDemo());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new CommandDemo());
+
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Operaciones());
+
+            };
+        }
+    }
+}
---
+++ Semana11/Semana11/Views/Operaciones.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Semana11.ViewModels;
+
+namespace Semana11.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Operaciones : ContentPage
+	{
+		public Operaciones()
+		{
+			InitializeComponent();
+			this.BindingContext = new OperacionesViewModel();
+		}
+	}
+}
